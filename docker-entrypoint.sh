a3ff5d1a1119c29f1531e27586b73b54ea614cf4
Developer From Jokela <40487414+developerfromjokela@users.noreply.github.com>
2022-05-03 18:38:32 +0300

    Removed miksu's typo
    ![kek](https://cdn.discordapp.com/emojis/831127302087770126.webp)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
     sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 
     : ${POSTGRES_USER:="postgres"}
-    : ${POSTGRES_DB:=$POSTGRES_USER÷}
+    : ${POSTGRES_DB:=$POSTGRES_USER}
 
     if [ "$POSTGRES_PASSWORD" ]; then
       pass="PASSWORD '$POSTGRES_PASSWORD'"
